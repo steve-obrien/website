4c961d34b8c43b284faac779533d85bb5a940e1e
Steve O'Brien <steve@newicon.net>
2024-04-08 09:14:08 +0100

    init

---
+++ app.vue
@@ -0,0 +1,21 @@
+<template>
+	<h1 class="text-3xl font-bold underline">
+		Steve O'Brien
+	</h1>
+</template>
+
+<script setup lang="ts">
+useHead({
+	title: 'Steve O\'Brien',
+	meta: [
+		{ name: 'description', content: 'My amazing site.' }
+	],
+	htmlAttrs: {
+		class: 'h-full'
+	},
+	bodyAttrs: {
+		class: 'font-sans antialiased text-black min-h-full flex flex-col'
+	},
+	script: [{ innerHTML: 'console.log(\'Hello world\')' }]
+})
+</script>
